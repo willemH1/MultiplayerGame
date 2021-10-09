89341ae77429590240f994530d4d9116883a4588
willemH <willem.hollants@hotmail.com>
2021-10-09 13:56:34 +0200

    Mirror

---
+++ MultiplayerTest3_clone_0/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class Player : NetworkBehaviour
+{
+    public float movementSpeed = 5f;
+    void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+            transform.position = transform.position + movement * movementSpeed;
+        }
+    }
+    private void Update()
+    {
+        HandleMovement();
+    }
+}
